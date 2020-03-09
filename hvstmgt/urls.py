0ba51d0ef9639e656ed4bc2069fab75b4cfdaeb1
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-03-09 19:34:45 +0530

    app initiated

--- hvstmgt/urls.py
+++ hvstmgt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from hvstmgtsystem import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('signup/', views.signup, name='signup'),
 ]

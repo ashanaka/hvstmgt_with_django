5af6118f3615eee542482340d4b63ce58fcb993e
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-03-09 20:14:46 +0530

    signup form

--- hvstmgt/urls.py
+++ hvstmgt/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from hvstmgtsystem import views
+from harvestMgtApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('signup/', views.signup, name='signup'),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
